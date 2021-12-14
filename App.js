4369888a0e1f0681d6f93fa8832bd10c71661228
Bhagyeshpro <bhagyeshpaunikar2001@gmail.com>
2021-12-14 14:41:04 -0800

    JS Is Done!

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, {useState} from "react";
+import { Button, SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const [gigs, setGigs] = useState([
+    {
+      description: "Freelancer.com",
+      amount: 399,
+    }
+  ])
+
+  const addGig = () => {
+    setGigs([
+      ...gigs,
+      {
+        description: description,
+        amount: amount,
+      }
+    ]);
+    setDescription("");
+    setAmount("");
+  }
+
   return (
+    <SafeAreaView>
     <View style={styles.container}>
-      <Text>React Native Basic Apps!</Text>
-      <StatusBar style="auto" />
+      <Text>React Native Gigs And Earning Counting Apps!</Text>
+      <TextInput 
+        placeholder="Enter Your Gig Name..." 
+        style={styles.gigInput}
+        value={description}
+        onChangeText={text => setDescription(text)}
+      />
+      <TextInput 
+        placeholder="Enter the amount you made in USD ..."
+        keyboardType="numeric"
+        style={styles.gigInput}
+        value={amount}
+        onChangeText={number => setAmount(number)}
+      />
+      <Button style={styles.addButton} disabled={!description && !amount} onPress={addGig} title="Add Gig"/>
+      {gigs.map(gig => (
+        <View>
+          <Text>{gig.description}</Text>
+          <Text>${gig.amount}</Text>
+        </View>
+      ))}
+
     </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,5 +60,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  addButton:{
+    padding: 20,
+  },
+  gigInput:{
+    borderRadius: 50,
+    width: "90%",
+    borderColor: "red",
+    borderWidth: 1,
+    margin: 30,
   },
 });
